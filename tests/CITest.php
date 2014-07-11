82af7e88ca583ccdd54df2a60a2e12bd7763da96
Nitin K Soni <soninitin27@gmail.com>
2014-07-11 15:35:04 +0530

    Initial Commit

---
+++ tests/CITest.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Description of CITest
+ *
+ * @author nick
+ */
+class CITest extends PHPUnit_Framework_TestCase
+{
+
+    private $CI;
+
+    public function setUp()
+    {
+        // Load CI instance normally
+        $this->CI = &get_instance();
+    }
+
+    public function testGetPost()
+    {
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+        $_GET['foo'] = 'bar';
+        $this->assertEquals('bar', $this->CI->input->get_post('foo'));
+    }
+
+}
